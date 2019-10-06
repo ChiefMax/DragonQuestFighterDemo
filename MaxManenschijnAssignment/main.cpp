e94fdfc4994d6e0f6f34b5a10df8ac9a492debc0
Max Manenschijn <maxmanenschijn@gmail.com>
2019-10-06 15:14:43 +0200

    Added a main menu screen with some buttons

--- MaxManenschijnAssignment/main.cpp
+++ MaxManenschijnAssignment/main.cpp
@@ -3,10 +3,30 @@
 
 int main() {
 
-	sf::RenderWindow ourWindow(sf::VideoMode(800, 600), "My Game");
+	sf::RenderWindow ourWindow(sf::VideoMode(1280, 720), "My Game");
 	
 	sf::CircleShape shape(50.f);
 	shape.setFillColor(sf::Color(100, 250, 50));
+	sf::Texture texture;
+	sf::Texture playT;
+	sf::Texture eraseT;
+	sf::Texture quitT;
+	if (!texture.loadFromFile("img/backgroundMainMenu.jpg"))
+		return EXIT_FAILURE;
+	if (!playT.loadFromFile("img/buttonStart.png"))
+		return EXIT_FAILURE;
+	if (!eraseT.loadFromFile("img/trashcan.png"))
+		return EXIT_FAILURE;
+	if (!quitT.loadFromFile("img/exit.png"))
+		return EXIT_FAILURE;
+	sf::Sprite backgroundMainMenu(texture);
+	sf::Sprite play(playT);
+	play.setPosition(sf::Vector2f(1100,150));
+	sf::Sprite erase(eraseT);
+	erase.setPosition(sf::Vector2f(1100, 250));
+	sf::Sprite quit(quitT);
+	quit.setPosition(sf::Vector2f(1100, 350));
+
 	while (ourWindow.isOpen())
 	{
 		sf::Event evnt;
@@ -20,7 +40,10 @@ int main() {
 			}
 		}
 		ourWindow.clear();
-		ourWindow.draw(shape);
+		ourWindow.draw(backgroundMainMenu);
+		ourWindow.draw(play);
+		ourWindow.draw(erase);
+		ourWindow.draw(quit);
 		ourWindow.display();
 	}
 }
