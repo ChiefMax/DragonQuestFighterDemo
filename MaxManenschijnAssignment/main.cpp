acfad13ba611180b4a44f1a2349c47a1fe0ccbec
Max <maxmanenschijn@gmail.com>
2019-10-13 15:34:13 +0200

    Basic set up done, will continue with this.

--- MaxManenschijnAssignment/main.cpp
+++ MaxManenschijnAssignment/main.cpp
@@ -1,36 +1,62 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
 #include "main.h"
+#include <Windows.h>
 #include "fightScreen.h"
+#include "scene.h"
+#include "spriteObject.hpp"
+#include "sceneHandler.h"
+#include <string>
+#include <iostream>
 
 int main() {
 
 	bool battleWindowOpen = false;
 	bool once = false;
+	int counter = 0;
 
 	sf::RenderWindow ourWindow(sf::VideoMode(1280, 720), "Dronken Quest");
+	Scene scene1("scene01");
+
+	SpriteObject sprite1("backgroundSprite", "img/backgroundMainMenu.png");
+	scene1.addGameObject(sprite1);
+
+	SpriteObject playSprite("playSprite", "img/buttonStart.png");
+	playSprite.setPosition(sf::Vector2f(1100.0f, 150.0f));
+	scene1.addGameObject(playSprite);
+
+	SpriteObject eraseSprite("eraseSprite", "img/trashcan.png");
+	eraseSprite.setPosition(sf::Vector2f(1100.0f, 250.0f));
+	scene1.addGameObject(eraseSprite);
+
+	SpriteObject quitSprite("quitSprite", "img/exit.png");
+	quitSprite.setPosition(sf::Vector2f(1100.0f, 350.0f));
+	scene1.addGameObject(quitSprite);
+
+	Scene scene2("scene02");
 
 	sf::CircleShape shape(50.f);
 	shape.setFillColor(sf::Color(100, 250, 50));
-	sf::Texture texture;
 	sf::Texture playT;
 	sf::Texture eraseT;
 	sf::Texture quitT;
-	if (!texture.loadFromFile("img/backgroundMainMenu.png"))
-		return EXIT_FAILURE;
 	if (!playT.loadFromFile("img/buttonStart.png"))
 		return EXIT_FAILURE;
 	if (!eraseT.loadFromFile("img/trashcan.png"))
 		return EXIT_FAILURE;
 	if (!quitT.loadFromFile("img/exit.png"))
 		return EXIT_FAILURE;
-	sf::Sprite backgroundMainMenu(texture);
 	sf::Sprite play(playT);
 	play.setPosition(sf::Vector2f(1100,150));
 	sf::Sprite erase(eraseT);
 	erase.setPosition(sf::Vector2f(1100, 250));
 	sf::Sprite quit(quitT);
 	quit.setPosition(sf::Vector2f(1100, 350));
+
+	SceneHandler handler;
+	handler.addScene(scene1);
+	handler.addScene(scene2);
 
 	while (ourWindow.isOpen())
 	{
@@ -43,12 +69,13 @@ int main() {
 			sf::FloatRect boundsPlayButton = play.getGlobalBounds();
 			sf::FloatRect boundsQuitButton = quit.getGlobalBounds();
 
-			// Going to the next screen.
+			//Going to the next screen.
 			if (boundsPlayButton.contains(mouse))
 			{
 				// mouse is on sprite!
 				battleWindowOpen = true;
 				once = true;
+				//counter++;
 			}
 
 			//Closing the game.
@@ -59,27 +86,39 @@ int main() {
 			}
 		}
 
+
 		sf::Event evnt;
 		while (ourWindow.pollEvent(evnt))
 		{
-			switch (evnt.type)
-			{
-			case sf::Event::Closed:ourWindow.close(); break;
-			default:
-				break;
+
+			if (battleWindowOpen && once) {
+				if (counter == 0) {
+					handler.stackScene("scene02");
+					counter++;
+					ourWindow.clear();
+				}
+				else {
+					handler.popScene();
+					counter--;
+				}
+				switch (evnt.type)
+				{
+				case sf::Event::Closed:ourWindow.close(); break;
+				default:
+					break;
+				}
 			}
 		}
 		ourWindow.clear();
-		ourWindow.draw(backgroundMainMenu);
-		ourWindow.draw(play);
-		ourWindow.draw(erase);
-		ourWindow.draw(quit);
+		//ourWindow.draw(play);
+		//ourWindow.draw(erase);
+		//ourWindow.draw(quit);
+		handler.update();
+		handler.render(ourWindow);
 		ourWindow.display();
 
 		if (battleWindowOpen && once) 
 		{
-			//FightWindow(shape);
-			int mF = OpenFightWindow();
 			once = false;
 		}
 	}
