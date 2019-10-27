c1b7563d8f2a647360d8787686e8fe817ec423db
Max <maxmanenschijn@gmail.com>
2019-10-27 16:04:32 +0100

    More development on turnbased combat and there is an option to continue after you defeated the enemy.

--- MaxManenschijnAssignment/main.h
+++ MaxManenschijnAssignment/main.h
@@ -10,5 +10,11 @@
 #include "quitButton.hpp"
 #include <string>
 #include <iostream>
+#include <fstream>
 
-void PlayerHandler(Button &attackButton, Character &character, Character &enemy, TextObject &hpTextEnemy, Button &defenceButton, TextObject &infoText, Button &recoverButton, TextObject &hpText, Button &magicButton, bool &specialMagic, bool &magicOnce, bool &EnemyTurn);
+//void PlayerHandler(Button &attackButton, Character &character, Character &enemy, TextObject &hpTextEnemy, Button &defenceButton, TextObject &infoText, Button &recoverButton, TextObject &hpText, Button &magicButton, bool &specialMagic, bool &magicOnce, bool &EnemyTurn);
+
+void EnemyHandler(TextObject &infoTextEnemy, Character &enemy, bool &PlayerTurn, bool &EnemyTurn, Character &character, TextObject &hpText);
+
+//void PlayerHandler(Button &attackButton, Character &character, Character &enemy, TextObject &hpTextEnemy, bool &EnemyTurn, bool &PlayerTurn, TextObject &infoText, Button &defenceButton, Button &recoverButton, TextObject &hpText, Button &magicButton, bool &specialMagic, bool &magicOnce, bool );
+void PlayerHandler(Button &attackButton, Character &character, Character &enemy, TextObject &hpTextEnemy, TextObject &attackTextEnemy, TextObject &defenceTextEnemy, bool &EnemyTurn, bool &PlayerTurn, TextObject &infoText, Button &defenceButton, Button &recoverButton, TextObject &hpText, Button &magicButton, bool &specialMagic, bool &magicOnce, bool &reset);
